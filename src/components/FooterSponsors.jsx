c575976afdb3364fef9133d89ad7de427cf99509
balderban <maria.perdomo@mocion.com.co>
2026-03-23 11:23:53 -0500

    quitando texto con referencia a AMD y texto de AMD en el fondo.

--- src/components/FooterSponsors.jsx
+++ src/components/FooterSponsors.jsx
@@ -4,7 +4,7 @@ import './footerSponsors.css';
 export default function FooterSponsors(){
   return (
     <div className="screen2__footer">
-      <img src={IMAGES.logosfooter} alt="AMD, Lenovo y Windows 11" />
+      <img src={IMAGES.logosfooter} alt="Intel, Lenovo y Windows 11" />
     </div>
   );
 }
